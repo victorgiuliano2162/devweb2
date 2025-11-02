e52d9cc19fbfb7b56ecd0eda8a04103d55cba0ff
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-02 18:52:53 -0300

    enums for status chamado

---
+++ app/src/main/java/com/docker/app/entities/enums/StatusChamado.java
@@ -0,0 +1,20 @@
+package com.docker.app.entities.enums;
+
+public enum StatusChamado {
+    ABERTO,
+    ATRIBUIDO,
+    EM_ANDAMENTO,
+    AGENDADO,
+    AGUARDANDO_CLIENTE,
+    AGUARDANDO_PECA,
+    AGUARDANDO_APROVACAO,
+    EM_ANALISE,
+    PENDENCIADO,
+    RESOLVIDO,
+    FINALIZADO,
+    CANCELADO,
+    REABERTO,
+    SUSPENSO,
+    FECHADO;
+}
+
