c8c0548cecaf6e30453ff201f91d57444d00521f
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-02 21:45:34 -0300

    funcionario repository

---
+++ app/src/main/java/com/docker/app/repositories/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.docker.app.repositories;
+
+import com.docker.app.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, String> {
+    Funcionario findByNome(String nome);
+    Funcionario findByEmail(String email);
+}
