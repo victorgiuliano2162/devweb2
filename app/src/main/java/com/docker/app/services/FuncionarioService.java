8cb2e383a72bbe041a59ac1d6298c6e8963c5ba5
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-03 21:34:29 -0300

    atualização na persistência da entidade funcionário

--- app/src/main/java/com/docker/app/services/FuncionarioService.java
+++ app/src/main/java/com/docker/app/services/FuncionarioService.java
@@ -1,9 +1,12 @@
 package com.docker.app.services;
 
 
+import com.docker.app.configuration.SecurityConfig;
 import com.docker.app.entities.Funcionario;
 import com.docker.app.repositories.FuncionarioRepository;
+import com.docker.app.repositories.SetorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +15,15 @@ public class FuncionarioService {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public Funcionario getFuncionarioPorId(String id) {
-        //TODO
         return funcionarioRepository.findById(id).get();
+    }
+
+    public Funcionario salvarFuncionario(Funcionario funcionario) {
+        funcionario.setSenha(passwordEncoder.encode(funcionario.getSenha()));
+        return  funcionarioRepository.save(funcionario);
     }
 }
