0cbe0faeeca80abcb01e8388b9c309dd82ff456c
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-05 10:10:33 -0300

    basic service for setor

---
+++ app/src/main/java/com/docker/app/services/SetorService.java
@@ -0,0 +1,19 @@
+package com.docker.app.services;
+
+
+import com.docker.app.entities.Setor;
+import com.docker.app.entities.enums.Setores;
+import com.docker.app.repositories.SetorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetorService {
+
+    @Autowired
+    private SetorRepository setorRepository;
+
+    public Setor findByNome(Setores nome) {
+        return setorRepository.findByNome(nome);
+    }
+}
