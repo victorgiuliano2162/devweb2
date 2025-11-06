8a7c7cafc5f9028adc1dadf89960334b6d85f8a3
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-06 11:43:59 -0300

    added Component anotation

--- app/src/main/java/com/docker/app/configuration/populateSQL/ChamadoTecnicoSQL.java
+++ app/src/main/java/com/docker/app/configuration/populateSQL/ChamadoTecnicoSQL.java
@@ -1,5 +1,8 @@
 package com.docker.app.configuration.populateSQL;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ChamadoTecnicoSQL {
     public void createChamados() {}
 }
