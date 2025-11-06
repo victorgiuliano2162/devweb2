6dc71ebfcd90925c0bf7856fec8c5ae5783154b9
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-11-06 18:02:17 -0300

    config for bralizian timezone

---
+++ app/src/main/java/com/docker/app/configuration/TimeZoneConfig.java
@@ -0,0 +1,14 @@
+package com.docker.app.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import java.util.TimeZone;
+
+@Configuration
+public class TimeZoneConfig {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
