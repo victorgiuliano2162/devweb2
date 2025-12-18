4089db533447c000f309ecefb6fa3b293d23fd6e
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-12-18 12:09:41 -0300

    propeties confiuratio annotation

--- app/src/main/java/com/docker/app/AppApplication.java
+++ app/src/main/java/com/docker/app/AppApplication.java
@@ -1,9 +1,12 @@
 package com.docker.app;
 
+import com.docker.app.configuration.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class AppApplication {
 
     public static void main(String[] args) {
