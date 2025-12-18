719c98184363ab16935f2180724a109d9db90494
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-12-18 12:08:35 -0300

    fix to accept Optional type

--- app/src/main/java/com/docker/app/repositories/FuncionarioRepository.java
+++ app/src/main/java/com/docker/app/repositories/FuncionarioRepository.java
@@ -4,10 +4,12 @@ import com.docker.app.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, String> {
-    Funcionario findByNome(String nome);
-    Funcionario findByEmail(String email);
 
+    Optional<Funcionario> findByNome(String nome);
+    Optional<Funcionario> findByEmail(String email);
 }
