3b34e62d1037178745db6dca5daaeea27ec945d1
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-12-19 00:42:19 -0300

    rollback with api parameters handlers

--- app/src/main/java/com/docker/app/controllers/ChamadoTecnicoController.java
+++ app/src/main/java/com/docker/app/controllers/ChamadoTecnicoController.java
@@ -8,7 +8,6 @@ import com.docker.app.repositories.ChamadoTecnicoRepository;
 import com.docker.app.services.ChamadoTecnicoService;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -42,25 +41,25 @@ public class ChamadoTecnicoController {
         return chamadoTecnicoService.criarChamadoTecnico(chamadoTecnico);
     }
 
-    @GetMapping
-    public Page<ChamadoTecnico> listarComFiltros(
-            @ModelAttribute @Valid ChamadoTecnicoFilter filter,
-            @PageableDefault(page = 0, size = 20, sort = "dataAbertura", direction = Sort.Direction.DESC) Pageable pageable) {
-
-        Specification<ChamadoTecnico> spec = (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            if (filter.getSetor() != null) predicates.add(cb.equal(root.get("setor"), filter.getSetor()));
-            if (filter.getTipoChamado() != null) predicates.add(cb.equal(root.get("tipoChamado"), filter.getTipoChamado()));
-            if (filter.getFuncId() != null) predicates.add(cb.equal(root.join("responsavelPelaAbertura").get("id"), filter.getFuncId()));
-            if (filter.getDate() != null) predicates.add(cb.equal(root.get("dataAbertura"), filter.getDate()));
-            if (filter.getAtivo() != null) predicates.add(cb.equal(root.get("ativo"), filter.getAtivo()));
-
-            return cb.and(predicates.toArray(new Predicate[0]));
-        };
-
-        return chamadoTecnicoService.findAllWithSpec(spec, pageable);
-    }
+//    @GetMapping
+//    public Page<ChamadoTecnico> listarComFiltros(
+//            @ModelAttribute @Valid ChamadoTecnicoFilter filter,
+//            @PageableDefault(page = 0, size = 20, sort = "dataCriacao", direction = Sort.Direction.DESC) Pageable pageable) {
+//
+//        Specification<ChamadoTecnico> spec = (root, query, cb) -> {
+//            List<Predicate> predicates = new ArrayList<>();
+//
+//            if (filter.getSetor() != null) predicates.add(cb.equal(root.get("setor"), filter.getSetor()));
+//            if (filter.getTipoChamado() != null) predicates.add(cb.equal(root.get("tipoChamado"), filter.getTipoChamado()));
+//            if (filter.getId() != null) predicates.add(cb.equal(root.join("responsavelPelaAbertura").get("id"), filter.getId()));
+//            if (filter.getDate() != null) predicates.add(cb.equal(root.get("dataAbertura"), filter.getDate()));
+//            if (filter.getAtivo() != null) predicates.add(cb.equal(root.get("ativo"), filter.getAtivo()));
+//
+//            return cb.and(predicates.toArray(new Predicate[0]));
+//        };
+//
+//        return chamadoTecnicoService.findAllWithSpec(spec, pageable);
+//    }
 
 
 //    @GetMapping
@@ -94,36 +93,37 @@ public class ChamadoTecnicoController {
 //    }
 //
 
-//    @GetMapping
-//    public Page<ChamadoTecnico> listarComFiltros(
-//            @RequestParam(required = false) Setores setor,
-//            @RequestParam(required = false) String funcId,
-//            @RequestParam(required = false) TipoChamado tipoChamado,
-//            @RequestParam(required = false) Boolean ativo,
-//            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-//            @PageableDefault(page = 0, size = 20) Pageable pageable) {
-//
-//        if (tipoChamado != null) {
-//            return chamadoTecnicoService.findByTipoChamado(tipoChamado, pageable);
-//        }
-//        if (setor != null) {
-//            return chamadoTecnicoService.findBySetor(setor, pageable);
-//        }
-//        if (funcId != null && date != null) {
-//            return chamadoTecnicoService.findResponsavelPelaAberturaPorData(funcId, date, pageable);
-//        }
-//        if (funcId != null) {
-//            return chamadoTecnicoService.findByResponsavelPelaAbertura(funcId, pageable);
-//        }
-//        if (ativo != null && ativo == true) {
-//            return chamadoTecnicoService.findByAtivoTrue(pageable);
-//        }
-//        if (ativo != null && !ativo) {
-//            return chamadoTecnicoService.findByAtivoFalse(pageable);
-//        }
-//
-//        return chamadoTecnicoService.findAll(pageable);
-//    }
+    @GetMapping
+    public Page<ChamadoTecnico> listarComFiltros(
+            @RequestParam(required = false) Setores setor,
+            @RequestParam(required = false) String id,
+            @RequestParam(required = false) TipoChamado tipoChamado,
+            @RequestParam(required = false) Boolean ativo,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @PageableDefault(page = 0, size = 20) Pageable pageable) {
+
+        if (ativo != null && ativo == true) {
+            return chamadoTecnicoService.findByAtivoTrue(pageable);
+        }
+        if (ativo != null && !ativo) {
+            return chamadoTecnicoService.findByAtivoFalse(pageable);
+        }
+        if (id != null && date != null) {
+            return chamadoTecnicoService.findResponsavelPelaAberturaPorData(id, date, pageable);
+        }
+        if (tipoChamado != null) {
+            return chamadoTecnicoService.findByTipoChamado(tipoChamado, pageable);
+        }
+        if (setor != null) {
+            return chamadoTecnicoService.findBySetor(setor, pageable);
+        }
+        if (id != null) {
+            return chamadoTecnicoService.findByResponsavelPelaAbertura(id, pageable);
+        }
+
+
+        return chamadoTecnicoService.findAll(pageable);
+    }
 
     @DeleteMapping
     public void deletarChamado(
