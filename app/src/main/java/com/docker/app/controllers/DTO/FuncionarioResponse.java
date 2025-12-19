d5a79a0ddcdc01894fc93fca38e3f328e9c38374
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-12-18 22:43:35 -0300

    improvments on dto

--- app/src/main/java/com/docker/app/controllers/DTO/FuncionarioResponse.java
+++ app/src/main/java/com/docker/app/controllers/DTO/FuncionarioResponse.java
@@ -1,10 +1,11 @@
 package com.docker.app.controllers.DTO;
 
 import com.docker.app.entities.Funcionario;
+import com.docker.app.entities.enums.CargoFuncionario;
 
-public record FuncionarioResponse(String id, String registro, String nome, String email, String telefone) {
+public record FuncionarioResponse(String id, String registro, String nome, String email, String telefone, CargoFuncionario cargo) {
     public static FuncionarioResponse fromEntity(Funcionario f) {
-        return new FuncionarioResponse(f.getId(), f.getRegistro(), f.getNome(), f.getEmail(), f.getTelefone());
+        return new FuncionarioResponse(f.getId(), f.getRegistro(), f.getNome(), f.getEmail(), f.getTelefone(), f.getCargo());
     }
 }
 
